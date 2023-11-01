6c6b3ca55b1374c0e9759fa018affec9a872ecd4
tyler36 <7234392+tyler36@users.noreply.github.com>
2023-11-01 01:23:10 +0000

    Using an icon font to display text symbols

--- app/Livewire/CreateCode.php
+++ app/Livewire/CreateCode.php
@@ -2,11 +2,51 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class CreateCode extends Component
 {
     public string $message;
+
+    protected array $symbols = [
+        'enable',
+        'public',
+        'grade',
+        'cookie',
+        'rocket',
+        'thunderstorm',
+        'face',
+        'skull',
+        'home',
+        'mode_cool',
+        'bedroom_baby',
+        'flatware',
+        'single_bed',
+        'sprinkler',
+        'umbrella',
+        'token',
+        'skillet',
+        'stadia_controller',
+        'airwave',
+        'floor_lamp',
+        'close',
+        'quiet_time',
+        'heat',
+        'tools_power_drill',
+        'nest_eco_leaf',
+        'air_freshener',
+    ];
+
+    #[Computed(persist: true)]
+    public function letters(): array
+    {
+        shuffle($this->symbols);
+
+        return collect(range('a', 'z'))
+            ->combine($this->symbols)
+            ->toArray();
+    }
 
     public function render()
     {
