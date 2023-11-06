d6a413b1a70c74b0c89837a4578a943e8ada9399
tyler36 <7234392+tyler36@users.noreply.github.com>
2023-11-06 01:54:13 +0000

    print friendly

--- resources/views/components/code.blade.php
+++ resources/views/components/code.blade.php
@@ -1,11 +1,15 @@
 @props(['message', 'letters'])
 
-<div class="flex justify-center gap-7">
+<div class="flex flex-wrap justify-center mt-auto gap-7">
     @foreach (explode(' ', $message) as $word)
-        <div class="word">
+        <div class="flex word gap-x-2">
             @foreach (str_split($word) as $character)
-                <span
-                    class="material-symbols-outlined character">{{ $letters[strtolower($character)] ?? $character }}</span>
+                <div>
+                    <span
+                        class="material-symbols-outlined character !text-4xl">{{ $letters[strtolower($character)] ?? $character }}
+                    </span>
+                    <div class="w-12 h-12 bg-gray-200 print:bg-black"></div>
+                </div>
             @endforeach
         </div>
     @endforeach
